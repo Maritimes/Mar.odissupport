ffe0349d5de764259541dbd675f8e56585df739f
McMahon <McMahonM@dfo-mpo.ca>
2018-06-12 15:24:15 -0300

    updated documentation

--- R/skipUselessRecs.R
+++ R/skipUselessRecs.R
@@ -1,3 +1,12 @@
+#' @title skipUselessRecs
+#' @description This function is used to prevent records from being sent to
+#' webservices if they do not contain anything that could be used to determine
+#' a definitive code.
+#' @param df This is a dataframe of species for which we have no definitive code
+#' @param field - field in the df containing the value to check against the 
+#' service
+#' @family speciesCodes
+#' @author  Mike McMahon, \email{Mike.McMahon@@dfo-mpo.gc.ca}
 skipUselessRecs <-function(df = NULL, field = NULL){
   skips = df[0,]
   if (nrow(df[is.na(df[,field]),])>0)skips = rbind(skips, df[is.na(df[,field]),])
